0479334f22a8e594802410f4402a6bb775779f9e
FAREAST\shrac <shrac@microsoft.com>
2016-10-28 16:11:51 +0530

    Minor change to throw exception upon client dispose

--- src/ResourceManagement/RecoveryServices.Backup/RecoveryServicesBackupManagement/Customizations/RecoveryServicesBackupManagementClient.cs
+++ src/ResourceManagement/RecoveryServices.Backup/RecoveryServicesBackupManagement/Customizations/RecoveryServicesBackupManagementClient.cs
@@ -16,6 +16,11 @@ namespace Microsoft.Azure.Management.RecoveryServices.Backup
     public partial class RecoveryServicesBackupManagementClient
     {
         /// <summary>
+        /// donot allow dispose incase the http client is shared.
+        /// </summary>
+        private bool donotAllowDispose = false;
+
+        /// <summary>
         /// Initializes a new instance of the
         /// RecoveryServicesBackupManagementClient class.
         /// </summary>
@@ -31,10 +36,10 @@ namespace Microsoft.Azure.Management.RecoveryServices.Backup
         /// <param name='httpClient'>
         /// The Http client
         /// </param>
-        /// <param name='donotModifyHttpClient'>
-        /// True if Http client is intended to be shared and not supposed to be modified with in the constructor.
+        /// <param name='donotAllowDisposeOnHttpClient'>
+        /// True if Http client is intended to be shared and not supposed to be disposed.
         /// </param>
-        public RecoveryServicesBackupManagementClient(SubscriptionCloudCredentials credentials, Uri baseUri, HttpClient httpClient, bool donotModifyHttpClient)
+        public RecoveryServicesBackupManagementClient(SubscriptionCloudCredentials credentials, Uri baseUri, HttpClient httpClient, bool donotAllowDisposeOnHttpClient)
             : base(httpClient)
         {
             if (credentials == null)
@@ -63,16 +68,25 @@ namespace Microsoft.Azure.Management.RecoveryServices.Backup
             this._longRunningOperationInitialTimeout = -1;
             this._longRunningOperationRetryTimeout = -1;
 
-            if (!donotModifyHttpClient)
-            {
-                // Set the http client timeout only if reuseHttpClient is false.
-                this.HttpClient.Timeout = TimeSpan.FromSeconds(300);
-            }
-
+            // Set if the underneath http client is supposed to be disposed or not.
+            this.donotAllowDispose = donotAllowDisposeOnHttpClient;
             this._credentials = credentials;
             this._baseUri = baseUri;
 
             this.Credentials.InitializeServiceClient(this);
+        }
+
+        /// <summary>
+        ///  Override the dispose method.
+        /// </summary>
+        public override void Dispose()
+        {
+            if (this.donotAllowDispose)
+            {
+                throw new Exception("Dispose is not allowed on this object as the HTTP client underneath is shared");
+            }
+
+            base.Dispose();
         }
     }
 }
